3694481432e92a1759996575d09a812094ed0cc4
edz1433 <edzavril1@gmail.com>
2024-11-15 14:09:30 +0800

    update

--- app/Models/Complaint.php
+++ app/Models/Complaint.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Complaint extends Model
 {
     use HasFactory;
-     protected $table = 'Complaint';
+     protected $table = 'complaint';
      protected $fillable =['complaint'];
 }
